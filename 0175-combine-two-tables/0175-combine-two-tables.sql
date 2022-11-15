0bfe3add80c0f146b499e867ad7b066447a57994
Gerald <geraldwogan@gmail.com>
2022-11-15 03:39:35 +0000

    Time: 547 ms (39.28%), Space: 0B (100.00%) - LeetHub

---
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -0,0 +1,10 @@
+# SELECT firstName, lastName
+# FROM Person
+# UNION ALL
+# SELECT city, IFNULL(state, NULL)
+# FROM Address
+
+
+SELECT p.firstName, p.lastName, a.city, a.state
+FROM Person as p
+LEFT JOIN Address as a ON a.personId =p.personId 
