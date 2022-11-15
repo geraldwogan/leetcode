00c75580161054b60341b55826066d382619f1b8
Gerald <geraldwogan@gmail.com>
2022-11-15 00:30:03 +0000

    Time: 477 ms (93.09%), Space: 0B (100.00%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,16 +1,3 @@
-# SELECT e.employee_id 
-# FROM Employees as e
-# JOIN Salaries as s ON e.employee_id = s.employee_id
-# WHERE e.name IS NULL
-# OR s.salary IS NULL
-
-# SELECT employee_id
-# FROM Employees as e
-# UNION 
-# SELECT employee_id
-# FROM Salaries as s
-# WHERE s.employee_id IN Employees
-
 SELECT employee_id
 FROM Employees
 WHERE employee_id NOT IN (SELECT employee_id FROM Salaries)
