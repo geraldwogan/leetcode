12c3ece2536e6f8decc921b2bc391dd15a15d5b6
Gerald <geraldwogan@gmail.com>
2024-06-11 22:46:21 +0100

    Time: 6596 ms (5.00%), Space: 18 MB (92.47%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,17 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        
+        # Implement Bubble Sort for this one
         for i in range(len(nums)):
             swapped = False
             for j in range(len(nums) - i - 1):
-                # print(f'{i} vs. {j}')
-                # print(f'({i}) {nums[i]} vs. ({j}) {nums[j]}')
                 if nums[j] == 0 and nums[j+1] != 0:
                     nums[j], nums[j+1] = nums[j+1], nums[j]
                     swapped = True
-
-                    # print('swap')
-                    # print(nums)
 
             if (swapped == False):
                 break
