9cb9670356bace72fc99d58d0222157ba8b045a0
Gerald <geraldwogan@gmail.com>
2024-06-11 23:27:06 +0100

    Time: 40 ms (35.68%), Space: 16.4 MB (83.45%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -4,34 +4,18 @@ class Solution:
         # base case:
         if len(nums) == 0:
             return 0
-        print(nums)
-        # Reverse bubble sort?
+
         for i in range(len(nums)-1, -1, -1):
-            # nums.remove()
-            print(i)
-            # HMmm... bubble sort
             swapped = False
             for j in range(len(nums)-i-2, -1, -1):
-                print(f'(j) {nums[j]} vs. {nums[j+1]}')
                 if nums[j] == val:
-                    print('swapa')
-                    # nums[j], nums[j+1] = nums[j+1], nums[j]
                     nums.remove(nums[j])
                 
             if swapped == True:
                 break
         if val in nums:
             nums.remove(val)
-        # for
-        # nums = nums.split(val)[0]
+
         return  len(nums)
-        # print(nums)
-        # # for i in range(len(nums )-1, -1, -1):
-        # keeps = []
-        # for i in range(len(nums)):
-        #     print(i)
-        #     if nums[i] != val:
-        #         keeps.append(i)
-        #         print(f'remove {i}')
-        #         nums.remove(nums[i])
-        # nums = nums[[1,3]]
+
+        
