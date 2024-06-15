25fd33230300ad8db1ee101693df96114de4b9e9
Gerald <geraldwogan@gmail.com>
2024-06-15 02:52:51 +0100

    Time: 68 ms (5.87%), Space: 16.7 MB (21.86%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -5,14 +5,9 @@ class Solution:
         
         for val in arr:
             x[val] = arr.count(val)
-            
-        print(x)
-        
+                    
         for k, val in enumerate(x):
-            print(k)
-            print(val)
-            print(x[val])
-            # print(x.values().remove(x[val]))
+
             if list(x.values()).count(x[val]) >  1:
                 return False
             
