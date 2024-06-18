13d907fac0fb189f7b75ac68995e8957be709eac
Gerald <geraldwogan@gmail.com>
2024-06-18 02:04:18 +0100

    Time: 90 ms (6.63%), Space: 16.6 MB (63.60%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -8,38 +8,27 @@ class Solution:
         
         while p2 < end:
             if(chars[p1] == chars[p2]): 
-                print(f'match: {chars[p1]} == {chars[p2]}')
+                # print(f'match: {chars[p1]} == {chars[p2]}')
                 p2 +=1 #TODO: Handle tens
             else:
                 s += chars[p1] 
                 s += str(p2-p1) if p2-p1>1 else ''
-                print(s)
+                # print(s)
                 p1 = p2
                 
         s += chars[p1] 
         s += str(p2-p1) if p2-p1>1 else ''
-        print(p2)
-        print(s)
-        
-        p1 = p2
-        
-        # r= [*s]
-        print(f'chars: {chars}')
+
+        # print(s)
+                
+        # print(f'chars: {chars}')
         chars.clear()
-        print(f'chars: {chars}')
 
         for i, val in enumerate(s):
-            # chars.insert(i, r[i])
-            
-            # print(f'replace {chars[i]} with {s[i]}')
-            print(f'insert {s[i]}')
+            # print(f'insert {s[i]}')
             chars.insert(i,s[i])
             # print(chars)
-        
-        # chars = chars[0:-1]
-        # print( [*s])
-        # chars.clear()
-        # chars = [*s]
-        print(chars)
+
+        # print(chars)
         return len(chars)
             
