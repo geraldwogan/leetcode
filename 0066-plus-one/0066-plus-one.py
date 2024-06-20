08407698f41de39ec993e592f5614186989e9d6c
Gerald <geraldwogan@gmail.com>
2024-06-21 00:07:21 +0100

    Time: 39 ms (39.76%), Space: 16.5 MB (70.62%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,14 +1,17 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         
-        d=map(str, digits)
+        # Map ints to str
+        d = map(str, digits)
+        # Make list into str, and then into int
         d = int(''.join(d))
+        # Increment int by 1
         d += 1
-        d = str(d)
-        d = list(d)
-        d=map(int, d)
+        # Create list from int (needs to be str first)
+        d = list(str(d))
+        # Map list of str to list of ints
+        d = map(int, d)
         
-        print(d)
-        print(type(d))
+        # print(d)
         
         return d
