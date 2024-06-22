d96e88b764166d0a5440312c62e766cd211fd3cf
Gerald <geraldwogan@gmail.com>
2024-06-22 01:16:44 +0100

    Time: 40 ms (81.62%), Space: 17.7 MB (30.29%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,21 +1,27 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         
-        s = s.lower()
-        s = [x for x in s if x.isalnum()]
-        s = ''.join(s)
-        # print(s)
+        # Solution 1 - Two Pointer
+#         s = s.lower()
+#         s = [x for x in s.lower() if x.isalnum()]
+#         s = ''.join(s)
+#         # print(s)
         
-        # two pointers
-        p1 = 0
-        p2 = len(s)-1
+#         # two pointers
+#         p1 = 0
+#         p2 = len(s)-1
         
-        while p1<p2:
-            if s[p1] != s[p2]:
-                return False
-            p1+=1
-            p2-=1
+#         while p1<p2:
+#             if s[p1] != s[p2]:
+#                 return False
+#             p1+=1
+#             p2-=1
             
-        return True
+#         return True
+    
+    # Solution 2 
+        s = [x for x in s.lower() if x.isalnum()]
+        return s == s[::-1]
+
         
         
